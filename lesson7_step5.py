26f874fd3f4ea0108517d40aa5b6466b1570874e
lst <lessprit@mail.ru>
2026-02-03 23:16:25 +0500

    Новый урок

---
+++ lesson7_step5.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+    #Считываем значение x
+    x_element = browser.find_element(By.ID, 'input_value')
+    x = x_element.text
+    y = calc(x)
+
+    #Вводим полученный ответ
+    answer = browser.find_element(By.ID, 'answer')
+    answer.send_keys(y)
+
+    #Отмечаем чекбокс и переключатель
+    options1 = browser.find_element(By.ID, 'robotCheckbox')
+    options1.click()
+    options2 = browser.find_element(By.ID, 'robotsRule')
+    options2.click()
+
+    #Отправляем
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()

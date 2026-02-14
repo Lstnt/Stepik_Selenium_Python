658530ad29bec82cd5ffb2a13b6a930e07490f5b
lst <lessprit@mail.ru>
2026-02-14 22:42:30 +0500

    Тема "Фикстуры"

--- Other/Интерпритатор.py
+++ Other/Интерпритатор.py
@@ -5,3 +5,19 @@ print(f"Let's count together:{str1}, then goes {str2}, and then {str3}")
 actual_result = "abrakadabra"
 print(f"Wrong text, got {actual_result}, something wrong")
 
+
+def decorator_function(func):
+    def wrapper():
+        print('Функция-обёртка!')
+        print('Оборачиваемая функция: {}'.format(func))
+        print('Выполняем обёрнутую функцию...')
+        func()
+        print('Выходим из обёртки')
+    return wrapper
+
+@decorator_function
+def hello_world():
+    print('Hello world!')
+
+hello_world()
+

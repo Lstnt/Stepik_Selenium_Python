c3895d0e3c1a165f5d896ac9cac2183ac47f98f9
lst <lessprit@mail.ru>
2026-02-22 19:25:29 +0500

    upd

--- User_language/test_items.py
+++ User_language/test_items.py
@@ -7,5 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_cart_button(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    assert len(browser.find_elements(By.CSS_SELECTOR, "button.btn-primary.btn-add-to-baske")) > 0, \
+    assert len(browser.find_elements(By.CSS_SELECTOR, "button.btn-primary.btn-add-to-basket")) > 0, \
         "Отсутствует кнопка добавления в корзину"
